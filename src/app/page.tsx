be79a25e682ab6ba954db28f5940c2def9dbdfdc
Daniel George <diggyzar@bu.edu>
2025-06-24 00:14:23 -0400

    used init variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,7 +159,7 @@ export default function Home() {
     }),
     [],
   );
-
+  if (init) {
   return (
     <>
       <div style={{position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: -1, pointerEvents: "none"}}>
@@ -184,5 +184,5 @@ export default function Home() {
         </Stack>
     </div>
     </>
-  );
+  )};
 }
